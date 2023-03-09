bd4fc7101e7962c061cefa7cb6f02d98db77849a
Valeryano <valeryanosantosa@webmail.umm.ac.id>
2023-03-09 08:25:54 +0700

    commit

---
+++ src/Modul1/Hidangan.java
@@ -0,0 +1,17 @@
+package Modul1;
+
+public class Hidangan {
+    protected String namaHIdangan;
+
+    public String getNamaHIdangan() {
+        return namaHIdangan;
+    }
+
+    public void setNamaHIdangan(String namaHIdangan) {
+        this.namaHIdangan = namaHIdangan;
+    }
+
+    public String disantap(){
+        return "Makanan dihidangkan";
+    }
+}
